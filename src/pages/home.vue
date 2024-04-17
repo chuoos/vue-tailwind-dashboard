f252d3095a771291a0f7e740101929bfe6161612
yunmi kim <poptoon@minisoft.co.kr>
2024-04-17 14:47:01 +0900

    breadcrumb 반영

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,22 +1,25 @@
 <template>
   <div>
-    <h2 class="text-2xl font-bold py-2">home</h2>
-    <nav aria-label="Breadcrumb pb-4">
-      <ol role="list" class="mx-auto flex items-center text-sm space-x-2">
-        <li class="flex items-center">
-          <a href="#" class="text-gray-900">depth1</a>
-          <svg width="16" height="20" viewBox="0 0 16 20" fill="currentColor" aria-hidden="true" class="h-auto w-4 text-gray-300">
-            <path d="M5.697 4.34L8.98 16.532h1.327L7.025 4.341H5.697z" />
-          </svg>
+    <h2 class="text-2xl font-bold py-2">Home</h2>
+
+    <nav aria-label="breadcrumb" class="w-full p-2 rounded-md bg-gray-100 text-gray-900">
+      <ol class="flex w-full flex-wrap items-center">
+        <li class="flex cursor-pointer items-center text-sm leading-normal antialiased transition-colors duration-300 hover:text-pink-500">
+          <a class="opacity-60" href="#"><span>Docs</span></a>
+          <span class="pointer-events-none mx-2 select-none text-sm leading-normal text-gray-500 antialiased">
+            /
+          </span>
         </li>
-        <li class="flex items-center">
-          <a href="#" class="text-gray-900">depth2</a>
-          <svg width="16" height="20" viewBox="0 0 16 20" fill="currentColor" aria-hidden="true" class="h-auto w-4 text-gray-300">
-            <path d="M5.697 4.34L8.98 16.532h1.327L7.025 4.341H5.697z" />
-          </svg>
+        <li class="flex cursor-pointer items-center text-sm leading-normal text-gray-900 antialiased transition-colors duration-300 hover:text-pink-500">
+          <a class="opacity-60" href="#"><span>Components</span></a>
+          <span class="pointer-events-none mx-2 select-none text-sm leading-normal text-gray-500 antialiased">
+            /
+          </span>
         </li>
-        <li class="flex items-center">
-          <a href="#" aria-current="page" class="text-gray-500 hover:text-gray-600">Home</a>
+        <li class="flex cursor-pointer items-center text-sm leading-normal text-gray-900 antialiased transition-colors duration-300 hover:text-pink-500">
+          <a class="text-gray-900 transition-colors hover:text-pink-500" href="#">
+            Breadcrumbs
+          </a>
         </li>
       </ol>
     </nav>
