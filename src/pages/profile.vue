fd2514d2bd3b78a45491ac203bb71d18a214e8ca
yunmi kim <poptoon@minisoft.co.kr>
2024-04-17 16:48:46 +0900

    컬러셋 가이드 화면 반영

--- src/pages/profile.vue
+++ src/pages/profile.vue
@@ -1,8 +1,120 @@
 <template>
-    <div>
+  <div>
     <h2 class="text-2xl font-bold py-2">profile</h2>
     <div class="overflow-x-auto relative">
-      
+      <div class="p-6">
+        <div class="grid grid-cols-10 gap-2">
+          <div class="bg-primary-50 aspect-square">primary</div>
+          <div class="bg-primary-100 aspect-square"></div>
+          <div class="bg-primary-200 aspect-square"></div>
+          <div class="bg-primary-300 aspect-square"></div>
+          <div class="bg-primary-400 aspect-square"></div>
+          <div class="bg-primary-500 aspect-square"></div>
+          <div class="bg-primary-600 aspect-square"></div>
+          <div class="bg-primary-700 aspect-square"></div>
+          <div class="bg-primary-800 aspect-square"></div>
+          <div class="bg-primary-900 aspect-square"></div>
+
+          <div class="bg-surface-50 aspect-square">surface</div>
+          <div class="bg-surface-100 aspect-square"></div>
+          <div class="bg-surface-200 aspect-square"></div>
+          <div class="bg-surface-300 aspect-square"></div>
+          <div class="bg-surface-400 aspect-square"></div>
+          <div class="bg-surface-500 aspect-square"></div>
+          <div class="bg-surface-600 aspect-square"></div>
+          <div class="bg-surface-700 aspect-square"></div>
+          <div class="bg-surface-800 aspect-square"></div>
+          <div class="bg-surface-900 aspect-square"></div>
+
+          <div class="bg-gray-50 aspect-square">gray</div>
+          <div class="bg-gray-100 aspect-square"></div>
+          <div class="bg-gray-200 aspect-square"></div>
+          <div class="bg-gray-300 aspect-square"></div>
+          <div class="bg-gray-400 aspect-square"></div>
+          <div class="bg-gray-500 aspect-square"></div>
+          <div class="bg-gray-600 aspect-square"></div>
+          <div class="bg-gray-700 aspect-square"></div>
+          <div class="bg-gray-800 aspect-square"></div>
+          <div class="bg-gray-900 aspect-square"></div>
+
+          <div class="bg-blue-50 aspect-square">blue</div>
+          <div class="bg-blue-100 aspect-square"></div>
+          <div class="bg-blue-200 aspect-square"></div>
+          <div class="bg-blue-300 aspect-square"></div>
+          <div class="bg-blue-400 aspect-square"></div>
+          <div class="bg-blue-500 aspect-square"></div>
+          <div class="bg-blue-600 aspect-square"></div>
+          <div class="bg-blue-700 aspect-square"></div>
+          <div class="bg-blue-800 aspect-square"></div>
+          <div class="bg-blue-900 aspect-square"></div>
+
+          <div class="bg-green-50 aspect-square">green</div>
+          <div class="bg-green-100 aspect-square"></div>
+          <div class="bg-green-200 aspect-square"></div>
+          <div class="bg-green-300 aspect-square"></div>
+          <div class="bg-green-400 aspect-square"></div>
+          <div class="bg-green-500 aspect-square"></div>
+          <div class="bg-green-600 aspect-square"></div>
+          <div class="bg-green-700 aspect-square"></div>
+          <div class="bg-green-800 aspect-square"></div>
+          <div class="bg-green-900 aspect-square"></div>
+
+          <div class="bg-yellow-50 aspect-square">yellow</div>
+          <div class="bg-yellow-100 aspect-square"></div>
+          <div class="bg-yellow-200 aspect-square"></div>
+          <div class="bg-yellow-300 aspect-square"></div>
+          <div class="bg-yellow-400 aspect-square"></div>
+          <div class="bg-yellow-500 aspect-square"></div>
+          <div class="bg-yellow-600 aspect-square"></div>
+          <div class="bg-yellow-700 aspect-square"></div>
+          <div class="bg-yellow-800 aspect-square"></div>
+          <div class="bg-yellow-900 aspect-square"></div>
+
+          <div class="bg-orange-50 aspect-square">orange</div>
+          <div class="bg-orange-100 aspect-square"></div>
+          <div class="bg-orange-200 aspect-square"></div>
+          <div class="bg-orange-300 aspect-square"></div>
+          <div class="bg-orange-400 aspect-square"></div>
+          <div class="bg-orange-500 aspect-square"></div>
+          <div class="bg-orange-600 aspect-square"></div>
+          <div class="bg-orange-700 aspect-square"></div>
+          <div class="bg-orange-800 aspect-square"></div>
+          <div class="bg-orange-900 aspect-square"></div>
+
+          <div class="bg-bluegray-50 aspect-square">bluegray</div>
+          <div class="bg-bluegray-100 aspect-square"></div>
+          <div class="bg-bluegray-200 aspect-square"></div>
+          <div class="bg-bluegray-300 aspect-square"></div>
+          <div class="bg-bluegray-400 aspect-square"></div>
+          <div class="bg-bluegray-500 aspect-square"></div>
+          <div class="bg-bluegray-600 aspect-square"></div>
+          <div class="bg-bluegray-700 aspect-square"></div>
+          <div class="bg-bluegray-800 aspect-square"></div>
+          <div class="bg-bluegray-900 aspect-square"></div>
+
+          <div class="bg-violet-50 aspect-square">violet</div>
+          <div class="bg-violet-100 aspect-square"></div>
+          <div class="bg-violet-200 aspect-square"></div>
+          <div class="bg-violet-300 aspect-square"></div>
+          <div class="bg-violet-400 aspect-square"></div>
+          <div class="bg-violet-500 aspect-square"></div>
+          <div class="bg-violet-600 aspect-square"></div>
+          <div class="bg-violet-700 aspect-square"></div>
+          <div class="bg-violet-800 aspect-square"></div>
+          <div class="bg-violet-900 aspect-square"></div>
+
+          <div class="bg-red-50 aspect-square">red</div>
+          <div class="bg-red-100 aspect-square"></div>
+          <div class="bg-red-200 aspect-square"></div>
+          <div class="bg-red-300 aspect-square"></div>
+          <div class="bg-red-400 aspect-square"></div>
+          <div class="bg-red-500 aspect-square"></div>
+          <div class="bg-red-600 aspect-square"></div>
+          <div class="bg-red-700 aspect-square"></div>
+          <div class="bg-red-800 aspect-square"></div>
+          <div class="bg-red-900 aspect-square"></div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -13,6 +125,4 @@ export default {
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
