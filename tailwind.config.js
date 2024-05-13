cf65105c55555e11fbf09385d929bd3739e98fca
yunmi kim <poptoon@minisoft.co.kr>
2024-05-13 17:24:55 +0900

    prettierrc 도전중

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,27 +3,31 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx,html}"],
-    theme: {
-      extend: {
-        colors: {
-          bluegray: {
-            "50": "#f7f8f9",
-            "100": "#dadee3",
-            "200": "#bcc3cd",
-            "300": "#9fa9b7",
-            "400": "#818ea1",
-            "500": "#64748b",
-            "600": "#556376",
-            "700": "#465161",
-            "800": "#37404c",
-            "900": "#282e38",
-          },
-        },
-        fontFamily: {
-          pretendard: ["Pretendard"],
+  theme: {
+    extend: {
+      colors: {
+        bluegray: {
+          "50": "#f7f8f9",
+          "100": "#dadee3",
+          "200": "#bcc3cd",
+          "300": "#9fa9b7",
+          "400": "#818ea1",
+          "500": "#64748b",
+          "600": "#556376",
+          "700": "#465161",
+          "800": "#37404c",
+          "900": "#282e38",
         },
       },
+      fontFamily: {
+        pretendard: ["Pretendard"],
+      },
     },
-  plugins: [],
+  },
+  plugins: [
+    // .prettierrc
+    {
+      "tailwindConfig": "tailwind.config.js"
+    }
+  ],
 }
-
